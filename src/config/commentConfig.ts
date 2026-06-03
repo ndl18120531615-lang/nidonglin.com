5b8c98b80018b3e92c2a1681ae0d4bb9ff7e4f7b
ndl18120531615-lang <ndl18120531615@gmail.com>
2026-06-03 22:31:46 +0800

    feat: Twikoo 使用自定义域名 twikoo.nidonglin.com + 实现 visitorCount 访客统计

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -6,7 +6,7 @@ export const commentConfig: CommentConfig = {
 
 	//twikoo评论系统配置
 	twikoo: {
-		envId: "https://ndl18120531615-twikoo.vercel.app",
+		envId: "https://twikoo.nidonglin.com",
 		// 设置 Twikoo 评论系统语言
 		lang: "zh-CN",
 		// 是否启用文章访问量统计功能
